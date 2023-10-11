c0e3444b1a3e4292521e4fa087874a3fb746baaf
Lucas Corrêa Agostinho <lucascagostinho@gmail.com>
2023-10-10 21:41:19 -0300

    Utilizacao do lombok, repository e ResponseEntity

--- todolist/src/main/java/br/com/lucascagostinho/todolist/user/UserController.java
+++ todolist/src/main/java/br/com/lucascagostinho/todolist/user/UserController.java
@@ -1,5 +1,8 @@
 package br.com.lucascagostinho.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +11,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+    @Autowired
+    private IUserRepository iUserRepository;
+
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.userName);
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+        var user = this.iUserRepository.findByUserName(userModel.getUserName());
+        if (user != null) {
+            return ResponseEntity.status(HttpStatus.BAD_GATEWAY).body("Usuário já existe");
+        }
+
+        var userCreated = this.iUserRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
